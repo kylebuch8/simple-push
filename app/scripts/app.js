59d751e227bec6ee51644c9a02577cd3dd6f9e2d
Kyle Buchanan <kylebuch8@gmail.com>
2015-12-09 16:00:07 -0500

    jshint fixes and added .jshintrc

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,34 +11,9 @@
     };
 
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/service-worker.js').then(initialize, function (err) {
+        navigator.serviceWorker.register('/service-worker.js').then(function () {}, function (err) {
             console.warn('Service workers aren\'t supported in this browser.');
             console.log(err);
         });
-    }
-
-    function initialize() {
-        // if (!('showNotification' in ServiceWorkerRegistration.prototype)) {
-        //     console.warn('Notifications aren\'t supported.');
-        //     return;
-        // }
-        //
-        // if (Notification.permission === 'denied') {
-        //     console.warn('The user has blocked notifications');
-        //     return;
-        // }
-        //
-        // if (!('PushManager' in window)) {
-        //     console.warn('Push Messaging isn\'t supported.');
-        //     return;
-        // }
-        //
-        // navigator.serviceWorker.ready.then(function (serviceWorkerRegistration) {
-        //     serviceWorkerRegistration.pushManager.subscribe({
-        //         userVisibleOnly: true
-        //     }).then(function (subscription) {
-        //         console.log('endpoint:', subscription.endpoint);
-        //     });
-        // });
     }
 }(document));
